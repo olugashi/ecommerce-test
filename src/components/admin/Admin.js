56dca003e7a4d694f8da240670dd2f2616074145
olugashi <44696376+olugashi@users.noreply.github.com>
2019-12-17 18:54:37 +0200

    fix error

--- src/components/admin/Admin.js
+++ src/components/admin/Admin.js
@@ -3,18 +3,18 @@ import React from "react";
 function LoginAdmin() {
   return (
     <div>
-  <div class="ui attached tabular menu">
-    <a class="active item">
-      <i aria-hidden="true" class="users icon"></i>
+  <div className="ui attached tabular menu">
+    <a className="active item">
+      <i aria-hidden="true" className="users icon"></i>
       Users
     </a>
-    <a class="test item">
+    <a className="test item">
       Messages
-      <div class="ui label">15</div>
+      <div className="ui label">15</div>
     </a>
   </div>
-  <div class="ui bottom attached segment active tab">Tab 1 Content</div>
-  <div class="ui bottom attached segment test tab">Tab 2 Content</div>
+  <div className="ui bottom attached segment active tab">Tab 1 Content</div>
+  <div className="ui bottom attached segment test tab">Tab 2 Content</div>
 </div>
   );
 }
