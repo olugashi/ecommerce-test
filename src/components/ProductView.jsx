377144de59908297ece4232f31b9a988cb6689d8
olugashi <44696376+olugashi@users.noreply.github.com>
2019-12-19 10:40:26 +0200

    Product View Clean code

--- src/components/ProductView.jsx
+++ src/components/ProductView.jsx
@@ -1,31 +1,19 @@
 import React, { useEffect } from "react";
-import { useSelector, useDispatch, useCallback } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import fetchProducts from "../services/fetchProductsService";
-import { getProducts } from "../reducers/fetchProductsReducer";
 
 function ProductView() {
   const dispatch = useDispatch();
   const products = useSelector(state => state.fetchProductsReducer.products);
 
-  //const [products, setProducts] = useSelector([]);
-
-  /*useEffect(() => {
-    dispatch(allActions.FetchProductsActions.f);
-    //const data1 = fetchProducts;
-    //console.log("useEffect " + data1);
-    //setProducts(data.Products);
-    //console.log(products);
-  }, []);*/
-
   function onFetchdata() {
-    //invoking action
     dispatch(fetchProducts());
   }
 
   useEffect(() => {
     console.log("useEffect " + products);
     onFetchdata();
-  }, [products]);
+  });
 
   return <div>products : {products}</div>;
 }
